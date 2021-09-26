377cc17655348b15e3ce521912c594c49d9db066
fantastas <msgitara@gmail.com>
2021-09-26 21:52:24 +0800

    cats servise uses interface, controler uses dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { CatsController } from './cats/cats.controller';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController, CatsController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,37 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  Redirect,
+  Req,
+} from '@nestjs/common';
 import { Request } from 'express';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private CatsService: CatsService) {}
   @Get()
-  findAll(@Req() request: Request): string {
-    return 'This returns all cats' + '\n' + request.headers.host;
+  async findAll(): Promise<Cat[]> {
+    return this.CatsService.findAll();
+  }
+
+  @Get('ab*cd')
+  async find() {
+    return 'This route uses a wildcard';
+  }
+
+  @Get(':id')
+  async findOne(@Param() params): Promise<string> {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
+  }
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    this.CatsService.create(createCatDto);
   }
 }
